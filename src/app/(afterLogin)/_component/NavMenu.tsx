1d3513ebe7805e6f1c51e8bfa8c184d389aa8346
umsungjun <umseongjun@naver.com>
2025-01-02 21:54:24 +0900

    Feat: NavMenu 컴포넌트 분리 및 마크업
    - segment 값에 따라 네비게이션 아이콘 Solid 처리

---
+++ src/app/(afterLogin)/_component/NavMenu.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import Link from "next/link";
+import { useSelectedLayoutSegment } from "next/navigation";
+
+/* Icon */
+import { HomeIcon } from "@heroicons/react/24/outline";
+import { HomeIcon as SolidHomeIcon } from "@heroicons/react/24/solid";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { MagnifyingGlassIcon as SolidMagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import { EnvelopeIcon } from "@heroicons/react/24/outline";
+import { EnvelopeIcon as SolidEnvelopeIcon } from "@heroicons/react/24/solid";
+import { UserCircleIcon } from "@heroicons/react/24/outline";
+import { UserCircleIcon as SolidUserCircleIcon } from "@heroicons/react/24/solid";
+
+export default function NavMenu() {
+  const segment = useSelectedLayoutSegment(); // ex) home
+  console.log(segment);
+  const me = {
+    // 임시 정보
+    id: "umsungjun",
+  };
+
+  return (
+    <div className="flex flex-col">
+      <Link href="/home" className="navMenu">
+        <>
+          {segment === "home" ? (
+            <SolidHomeIcon className="w-6" />
+          ) : (
+            <HomeIcon className="w-6" />
+          )}
+          <span className={segment === "home" ? "font-bold" : ""}>홈</span>
+        </>
+      </Link>
+      <Link href="/explore" className="navMenu">
+        {segment === "explore" ? (
+          <SolidMagnifyingGlassIcon className="w-6" />
+        ) : (
+          <MagnifyingGlassIcon className="w-6" />
+        )}
+        <span className={segment === "explore" ? "font-bold" : ""}>
+          탐색하기
+        </span>
+      </Link>
+      <Link href="/messages">
+        <div className="navMenu">
+          {segment === "messages" ? (
+            <SolidEnvelopeIcon className="w-6" />
+          ) : (
+            <EnvelopeIcon className="w-6" />
+          )}
+          <span className={segment === "messages" ? "font-bold" : ""}>
+            쪽지
+          </span>
+        </div>
+      </Link>
+      {me.id && (
+        <Link href={`/${me?.id}`}>
+          <div className="navMenu">
+            {segment === me.id ? (
+              <SolidUserCircleIcon className="w-6" />
+            ) : (
+              <UserCircleIcon className="w-6" />
+            )}
+            <span className={segment === me.id ? "font-bold" : ""}>프로필</span>
+          </div>
+        </Link>
+      )}
+    </div>
+  );
+}
